3443794ffaeaae86488ff318b6179346a7711ad8
sonnevaly <valygamer999@gmail.com>
2025-10-20 11:00:00 +0700

    first commit

---
+++ Lab_Class/lib/ui/quiz_console.dart
@@ -0,0 +1,35 @@
+import 'dart:io';
+
+import '../domain/quiz.dart';
+
+class QuizConsole {
+  Quiz quiz;
+
+  QuizConsole({required this.quiz});
+
+  void startQuiz() {
+    print('--- Welcome to the Quiz ---\n');
+
+    for (var question in quiz.questions) {
+      print('Question: ${question.title}');
+      print('Choices: ${question.choices}');
+      stdout.write('Your answer: ');
+      String? userInput = stdin.readLineSync();
+
+      // Check for null input
+      if (userInput != null && userInput.isNotEmpty) {
+        Answer answer = Answer(question: question, answerChoice: userInput);
+        quiz.addAnswer(answer);
+      } else {
+        print('No answer entered. Skipping question.');
+      }
+
+      print('');
+    }
+
+    int score = quiz.getScoreInPercentage();
+    print('--- Quiz Finished ---');
+    print('Your score: $score % correct');
+  }
+}
+ 
